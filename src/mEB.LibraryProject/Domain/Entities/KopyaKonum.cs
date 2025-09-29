53cc48f2cb1a322817002aa345c14e560cf8d4f9
emrealkan884 <hasan.alkan884@gmail.com>
2025-09-29 12:09:25 +0300

    Code Refactoring

--- src/mEB.LibraryProject/Domain/Entities/KopyaKonum.cs
+++ src/mEB.LibraryProject/Domain/Entities/KopyaKonum.cs
@@ -1,17 +1,29 @@
-﻿namespace Domain.Entities;
+﻿using NArchitecture.Core.Persistence.Repositories;
 
-public class KopyaKonum
+namespace Domain.Entities;
+
+public class KopyaKonum : Entity<Guid>
 {
-    public Guid Id { get; set; }
+    public KopyaKonum()
+    {
+    }
+
+    public KopyaKonum(Guid kopyaId, Guid konumId, Guid kutuphaneId, int adet)
+    {
+        KopyaId = kopyaId;
+        KonumId = konumId;
+        KutuphaneId = kutuphaneId;
+        Adet = adet;
+    }
 
     public Guid KopyaId { get; set; }
-    public Kopya Kopya { get; set; }
-
     public Guid KonumId { get; set; }
-    public Konum Konum { get; set; }
-
     public Guid KutuphaneId { get; set; }
-    public Kutuphane Kutuphane { get; set; }
-
     public int Adet { get; set; } = 1; // Bu konumda kaç kopya var
+
+
+    //Navigation
+    public Kopya Kopya { get; set; }
+    public Konum Konum { get; set; }
+    public Kutuphane Kutuphane { get; set; }
 }
