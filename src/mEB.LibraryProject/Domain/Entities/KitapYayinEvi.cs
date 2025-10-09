02e9cccb9c6f7bec3758792a072be10706218cb0
emrealkan884 <hasan.alkan884@gmail.com>
2025-10-09 10:32:21 +0300

    Refactorin with Cursor

---
+++ src/mEB.LibraryProject/Domain/Entities/KitapYayinEvi.cs
@@ -0,0 +1,19 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class KitapYayinEvi : Entity<Guid>
+{
+    public KitapYayinEvi() { }
+
+    public KitapYayinEvi(Guid kitapId, Guid yayinEviId)
+    {
+        KitapId = kitapId;
+        YayinEviId = yayinEviId;
+    }
+
+    public Guid KitapId { get; set; }
+    public Guid YayinEviId { get; set; }
+
+    // Legacy join entity kept for backward compatibility
+}
